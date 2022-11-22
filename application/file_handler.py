e361f8b9d7c87d4082db6b9bd8b7514b24c8cfa6
Ryan Haynes <50088158+ryanhaynes01@users.noreply.github.com>
2022-11-22 22:59:29 +0000

    Creation and early development of a file handler
    Basic file handler to manage all saving, loading, deletion, and general path generation for the program

---
+++ application/file_handler.py
@@ -0,0 +1,51 @@
+import os
+import sys
+
+class FileManager():
+    """
+    Class to manage files, handling errors, adding cross platform support.
+
+    Args:
+        None
+
+    Attributes:
+        cwd (str): Current working directory for file naviation
+    """
+
+    def __init__(self):
+        self.cwd = os.getcwd()
+
+    def create_path_string(self, *args: str) -> str:
+        """
+        Safely convert inputs into a path string, ensuring formatting
+        isn't an issue across different OS
+
+        Args:
+            *args (strs): collection of strings to be combined for a directory/file path
+
+        Returns:
+            final_path (str): string for new directory building from the current working directory
+        """
+        final_path = self.cwd
+
+        for child in args:
+            final_path = os.path.join(final_path, child)
+
+        return final_path
+
+    def load_file(self):
+        pass
+
+    def make_file(self):
+        pass
+
+    def delete_file(self):
+        pass
+
+    def save(self, data, location: str, file_extension: str) -> None:
+        pass
+
+
+if __name__ == "__main__":
+    fm = FileManager()
+    
